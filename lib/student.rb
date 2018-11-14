8f3d23e85ec557e69faa869585ceee28b5d19d84
Leifur Sigurdsson <leifurerik@gmail.com>
2018-11-14 22:33:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,9 +20,8 @@ class Student
   end
 
   def self.drop_table
-    sql = <<-SQL
-      DROP TABLE students
-    SQL
+    sql = "DROP TABLE IF EXISTS students"
+    
     DB[:conn].execute(sql)
   end
 
